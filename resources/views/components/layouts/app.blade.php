e2c808232b672ba4f7d29525067cbd47065a3734
Gungcakra <agungcakra888@gmail.com>
2025-03-22 14:57:52 +0800

    Refactor login and layout views; update form actions and remove unused elements

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -60,14 +60,14 @@
     </div>
     <!--end::Root-->
     <!--begin::Javascript-->
+    <script src="{{ asset('assets/plugins/global/plugins.bundle.js')}}"></script>
+    <script src="{{ asset('assets/js/scripts.bundle.js')}}"></script>
+    <script src="{{ asset('assets/js/custom/authentication/sign-in/general.js')}}"></script>
     <script>
         var hostUrl = "{{ asset('assets/')}}";
         
         </script>
     <!--begin::Global Javascript Bundle(mandatory for all pages)-->
-    <script src="{{ asset('assets/plugins/global/plugins.bundle.js')}}"></script>
-    <script src="{{ asset('assets/js/scripts.bundle.js')}}"></script>
-    <script src="{{ asset('assets/js/custom/authentication/sign-in/general.js')}}"></script>
    
     @livewireScripts
 </body>
--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -6,7 +6,7 @@
             <!--begin::Wrapper-->
             <div class="w-lg-500px p-10">
                 <!--begin::Form-->
-                <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" data-kt-redirect-url="../../demo1/dist/index.html" action="#">
+                <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" data-kt-redirect-url="{{ route('dashboard') }}" action="#">
                     <!--begin::Heading-->
                     <div class="text-center mb-11">
                         <!--begin::Title-->
@@ -18,12 +18,12 @@
                     </div>
                     <!--begin::Heading-->
                     <!--begin::Login options-->
-                    {{-- <div class="row g-3 mb-9">
+                    <div class="row g-3 mb-9">
                         <!--begin::Col-->
-                        <div class="col-md-6">
+                        {{-- <div class="col-md-6">
                             <!--begin::Google link=-->
                             <a href="#" class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
-                            <img alt="Logo" src="{{ asset('assets/media/svg/brand-logos/google-icon.svg')}}" class="h-15px me-3" />Sign in with Google</a>
+                            <img alt="Logo" src="assets/media/svg/brand-logos/google-icon.svg" class="h-15px me-3" />Sign in with Google</a>
                             <!--end::Google link=-->
                         </div>
                         <!--end::Col-->
@@ -31,12 +31,12 @@
                         <div class="col-md-6">
                             <!--begin::Google link=-->
                             <a href="#" class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
-                            <img alt="Logo" src="{{ asset('assets/media/svg/brand-logos/apple-black.svg')}}" class="theme-light-show h-15px me-3" />
-                            <img alt="Logo" src="{{ asset('assets/media/svg/brand-logos/apple-black-dark.svg')}}" class="theme-dark-show h-15px me-3" />Sign in with Apple</a>
+                            <img alt="Logo" src="assets/media/svg/brand-logos/apple-black.svg" class="theme-light-show h-15px me-3" />
+                            <img alt="Logo" src="assets/media/svg/brand-logos/apple-black-dark.svg" class="theme-dark-show h-15px me-3" />Sign in with Apple</a>
                             <!--end::Google link=-->
-                        </div>
+                        </div> --}}
                         <!--end::Col-->
-                    </div> --}}
+                    </div>
                     <!--end::Login options-->
                     <!--begin::Separator-->
                     {{-- <div class="separator separator-content my-14">
@@ -44,20 +44,15 @@
                     </div> --}}
                     <!--end::Separator-->
                     <!--begin::Input group=-->
-
-                    <form wire:submit.prevent="login">
-
                     <div class="fv-row mb-8">
                         <!--begin::Email-->
-                        <input type="text" wire:model.defer="email" placeholder="Email" name="email" autocomplete="off" class="form-control bg-transparent" id="email" />
-                        @error('email') <span class="text-red-500">{{ $message }}</span> @enderror
+                        <input type="text" placeholder="Email" name="email" autocomplete="off" class="form-control bg-transparent" wire:model="email" />
                         <!--end::Email-->
                     </div>
                     <!--end::Input group=-->
                     <div class="fv-row mb-3">
                         <!--begin::Password-->
-                        <input type="password" wire:model.defer="password" placeholder="Password" name="password" autocomplete="off" class="form-control bg-transparent" id="password"/>
-                        @error('password') <span class="text-red-500">{{ $message }}</span> @enderror
+                        <input type="password" placeholder="Password" name="password" autocomplete="off" class="form-control bg-transparent" wire:model="password" />
                         <!--end::Password-->
                     </div>
                     <!--end::Input group=-->
@@ -65,14 +60,13 @@
                     <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
                         <div></div>
                         <!--begin::Link-->
-                        <s href="../../demo1/dist/authentication/layouts/corporate/reset-password.html" class="link-primary">Forgot Password ?</s>
+                        {{-- <a href="../../demo1/dist/authentication/layouts/corporate/reset-password.html" class="link-primary">Forgot Password ?</a> --}}
                         <!--end::Link-->
                     </div>
-
                     <!--end::Wrapper-->
                     <!--begin::Submit button-->
                     <div class="d-grid mb-10">
-                        <button type="submit" wire:click.prevent="login" class="btn btn-primary">
+                        <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
                             <!--begin::Indicator label-->
                             <span class="indicator-label">Sign In</span>
                             <!--end::Indicator label-->
@@ -82,14 +76,11 @@
                             <!--end::Indicator progress-->
                         </button>
                     </div>
-                </form>
-
-
-
                     <!--end::Submit button-->
                     <!--begin::Sign up-->
-                    <div class="text-gray-500 text-center fw-semibold fs-6">Not a Member yet?
-                    <a class="link-primary">Sign up</a></div>
+                    <div class="text-gray-500 text-center fw-semibold fs-6">
+                    {{-- <a href="../../demo1/dist/authentication/layouts/corporate/sign-up.html" class="link-primary">Sign up</a> --}}
+                </div>
                     <!--end::Sign up-->
                 </form>
                 <!--end::Form-->
@@ -98,82 +89,7 @@
         </div>
         <!--end::Form-->
         <!--begin::Footer-->
-        <div class="w-lg-500px d-flex flex-stack px-10 mx-auto">
-            <!--begin::Languages-->
-            <div class="me-10">
-                <!--begin::Toggle-->
-                <button class="btn btn-flex btn-link btn-color-gray-700 btn-active-color-primary rotate fs-base" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start" data-kt-menu-offset="0px, 0px">
-                    <img data-kt-element="current-lang-flag" class="w-20px h-20px rounded me-3" src="{{ asset('assets/media/flags/united-states.svg')}}" alt="" />
-                    <span data-kt-element="current-lang-name" class="me-1">English</span>
-                    <span class="d-flex flex-center rotate-180">
-                        <i class="ki-duotone ki-down fs-5 text-muted m-0"></i>
-                    </span>
-                </button>
-                <!--end::Toggle-->
-                <!--begin::Menu-->
-                <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-semibold w-200px py-4 fs-7" data-kt-menu="true" id="kt_auth_lang_menu">
-                    <!--begin::Menu item-->
-                    <div class="menu-item px-3">
-                        <a href="#" class="menu-link d-flex px-5" data-kt-lang="English">
-                            <span class="symbol symbol-20px me-4">
-                                <img data-kt-element="lang-flag" class="rounded-1" src="{{ asset('assets/media/flags/united-states.svg')}}" alt="" />
-                            </span>
-                            <span data-kt-element="lang-name">English</span>
-                        </a>
-                    </div>
-                    <!--end::Menu item-->
-                    <!--begin::Menu item-->
-                    <div class="menu-item px-3">
-                        <a href="#" class="menu-link d-flex px-5" data-kt-lang="Spanish">
-                            <span class="symbol symbol-20px me-4">
-                                <img data-kt-element="lang-flag" class="rounded-1" src="{{ asset('assets/media/flags/spain.svg')}}" alt="" />
-                            </span>
-                            <span data-kt-element="lang-name">Spanish</span>
-                        </a>
-                    </div>
-                    <!--end::Menu item-->
-                    <!--begin::Menu item-->
-                    <div class="menu-item px-3">
-                        <a href="#" class="menu-link d-flex px-5" data-kt-lang="German">
-                            <span class="symbol symbol-20px me-4">
-                                <img data-kt-element="lang-flag" class="rounded-1" src="{{ asset('assets/media/flags/germany.svg')}}" alt="" />
-                            </span>
-                            <span data-kt-element="lang-name">German</span>
-                        </a>
-                    </div>
-                    <!--end::Menu item-->
-                    <!--begin::Menu item-->
-                    <div class="menu-item px-3">
-                        <a href="#" class="menu-link d-flex px-5" data-kt-lang="Japanese">
-                            <span class="symbol symbol-20px me-4">
-                                <img data-kt-element="lang-flag" class="rounded-1" src="{{ asset('assets/media/flags/japan.svg')}}" alt="" />
-                            </span>
-                            <span data-kt-element="lang-name">Japanese</span>
-                        </a>
-                    </div>
-                    <!--end::Menu item-->
-                    <!--begin::Menu item-->
-                    <div class="menu-item px-3">
-                        <a href="#" class="menu-link d-flex px-5" data-kt-lang="French">
-                            <span class="symbol symbol-20px me-4">
-                                <img data-kt-element="lang-flag" class="rounded-1" src="{{ asset('assets/media/flags/france.svg')}}" alt="" />
-                            </span>
-                            <span data-kt-element="lang-name">French</span>
-                        </a>
-                    </div>
-                    <!--end::Menu item-->
-                </div>
-                <!--end::Menu-->
-            </div>
-            <!--end::Languages-->
-            <!--begin::Links-->
-            <div class="d-flex fw-semibold text-primary fs-base gap-5">
-                {{-- <a href="../../demo1/dist/pages/team.html" target="_blank">Terms</a>
-                <a href="../../demo1/dist/pages/pricing/column.html" target="_blank">Plans</a>
-                <a href="../../demo1/dist/pages/contact.html" target="_blank">Contact Us</a> --}}
-            </div>
-            <!--end::Links-->
-        </div>
+       
         <!--end::Footer-->
     </div>
     <!--end::Body-->
