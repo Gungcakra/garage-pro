a1751ed43c747955ee907b58bda77d53de1b0ee8
Gungcakra <agungcakra888@gmail.com>
2025-03-29 12:11:57 +0800

    Add Roles and Permissions management component and update routes; include Spatie Laravel Permission package

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Livewire\Auth\Login;
 use App\Livewire\Customer;
 use App\Livewire\Dashboard;
 use App\Livewire\MenuManagement;
+use App\Livewire\RolesPermissions;
 use App\Livewire\Service;
 use App\Livewire\ServiceOperational;
 use App\Livewire\Sparepart as LivewireSparepart;
@@ -20,6 +21,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/employee', EmployeeManagement::class)->name('employee');
     Route::get('/menu', MenuManagement::class)->name('menu');
     Route::get('/service', Service::class)->name('service');
+    Route::get('/roles',RolesPermissions::class)->name('role');
     Route::get('/sparepart', LivewireSparepart::class)->name('sparepart');
     Route::get('/serviceoperational', ServiceOperational::class)->name('serviceoperational');
     Route::get('/serviceinvoice/{id}', [ServiceOperational::class, 'getInvoice'])->name('serviceinvoice');
