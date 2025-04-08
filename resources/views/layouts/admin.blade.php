463c87ddb4eb1d6a00909d8c6f1f30ef66f05e26
Gungcakra <agungcakra888@gmail.com>
2025-04-08 15:34:15 +0800

    fix: Update QR scanner script tag to ensure proper loading

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -95,7 +95,7 @@
     <!--begin::Javascript-->
     <script data-navigate-once src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script data-navigate-once src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-    <script data-navigate-once src="https://unpkg.com/qr-scanner@1.4.2/qr-scanner.legacy.min.js"></script>
+    <script src="https://unpkg.com/qr-scanner@1.4.2/qr-scanner.legacy.min.js"></script>
 
     <script>
         document.addEventListener('livewire:init', function() {
